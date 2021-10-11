d8f3efc6346d6ec9be70c2da57db2e7f68ab3664
Mikita.Nikolski <Mikita.Nikolski@Connectwise.com>
2021-10-11 12:59:28 +0300

    add: logout

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,9 @@
 const router = require('express').Router()
 
+const verifyToken = require('../middlewares/verifyToken')
 const authController = require('../controllers/auth')
+
+router.get('/logout', verifyToken, authController.logoutUser)
 
 router.post('/signUp', authController.signUpUser)
 router.post('/signIn', authController.signInUser)
